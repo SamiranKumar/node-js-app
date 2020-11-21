2c035b8976b97e3101a11a7689ab6866dd66cd40
samirankumar11 <samirankumar11@gmail.com>
2020-11-21 18:14:08 +0600

    Apply diffrend syntext for response and use db connection createPool funtion

--- app.js
+++ app.js
@@ -1,90 +1,136 @@
-const msql = require("mysql");
+const { query, queryCb } = require("./db");
 
 const express = require("express");
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+///
+////
+////
+app.get("/employees", (req, res) => {
+  let q = "SELECT * FROM employee";
 
-//===========================================================
-var mysqlConnection = msql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'employeedb',
-    multipleStatements:true  //For use store procedure
-
+  dbQuary(q)
+    .then(function (emp) {
+      console.log("emp:", emp);
+      res.send(emp);
+    })
+    .catch((err) => {
+      console.error("ERROR:" + err);
+      res.status(400).send(err.message);
+    });
 });
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("DB Connection Succcess!");
+app.get("/employees1", async (req, res) => {
+  let q = "SELECT * FROM employee";
+  try {
+    const emp = await dbQuary(q);
+    console.log("emp:", emp);
+    res.send(emp);
+  } catch (ex) {
+    console.error("ERROR:" + ex);
+    res.status(400).send(ex.message);
+  }
+});
+
+app.get("/employees2", (req, res) => {
+  let q = "SELECT * FROM employee";
+  queryCb(q, function (err, results) {
+    if (err) {
+      console.error("ERROR:" + err);
+      res.status(400).send(err.message);
     } else {
-        console.log("DB Connection Failed:" + err.message);
+      console.log("emp:", results);
+      res.send(results);
     }
+  });
 });
 
-
-app.listen(4000,()=>{
-    console.log("Express server is Running Port:4000");
+app.listen(4000, () => {
+  console.log("Express server is Running Port:4000");
 });
 
+// //===========================================================
+// var mysqlConnection = msql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "employeedb",
+//   multipleStatements: true, //For use store procedure
+// });
 
+// mysqlConnection.connect((err) => {
+//   if (!err) {
+//     console.log("DB Connection Succcess!");
+//   } else {
+//     console.log("DB Connection Failed:" + err.message);
+//   }
+// });
 
-app.get('/employees',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM employee',(err,rows,fields)=>{
-        if(!err){
-            //console.log(rows);
-        res.send(rows);
+// app.get("/employees", (req, res) => {
+//   mysqlConnection.query("SELECT * FROM employee", (err, rows, fields) => {
+//     if (!err) {
+//       //console.log(rows);
+//       //  res.send(rows);
+//       res.json(rows);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
 
-        }else{
-            console.log(err);
-        }
-    })
-});
+// app.get("/employees:id", (req, res) => {
+//   mysqlConnection.query(
+//     "SELECT * FROM employee WHERE id = ?",
+//     [req.params.id],
+//     (err, rows, fields) => {
+//       if (!err) {
+//         //console.log(rows);
 
-app.get('/employees:id',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM employee WHERE id = ?',[req.params.id],(err,rows,fields)=>{
-        if(!err){
-            //console.log(rows);
-        
-        res.send(rows);
-        }else{
-            console.log(err);
-        }
-    })
-});
+//         res.send(rows);
+//       } else {
+//         console.log(err);
+//       }
+//     }
+//   );
+// });
 
+// app.delete("/employees:id", (req, res) => {
+//   mysqlConnection.query(
+//     "DELETE FROM employee WHERE id = ?",
+//     [req.params.id],
+//     (err, rows, fields) => {
+//       if (!err) {
+//         //console.log(rows);
 
+//         res.send("DELETE SUCCESSFULLY");
+//       } else {
+//         console.log(err);
+//       }
+//     }
+//   );
+// });
 
-app.delete('/employees:id',(req,res)=>{
-    mysqlConnection.query('DELETE FROM employee WHERE id = ?',[req.params.id],(err,rows,fields)=>{
-        if(!err){
-            //console.log(rows);
-        
-        res.send("DELETE SUCCESSFULLY");
+// app.post("/employees", (req, res) => {
+//   let emp = req.body;
+//   console.log(emp);
+//   var sql =
+//     "SET @id = ?; SET @emp_name = ?; @emp_code = ?; @salary = ?; CALL Employee_ADD_OR_EDIT( @_id, @_emp_name, @_emp_code, @_salary)";
 
-        }else{
-            console.log(err);
-        }
-    })
-});
+//   mysqlConnection.query(
+//     sql,
+//     [emp.id, emp.name, emp.emp_code, emp.salary],
+//     (err, rows, fields) => {
+//       if (!err) {
+//         //console.log(rows);
 
-
-app.post('/employees',(req,res)=>{
-
-    let emp = req.body;
-    console.log(emp);
-    var sql = "SET @id = ?; SET @emp_name = ?; @emp_code = ?; @salary = ?; CALL Employee_ADD_OR_EDIT( @_id, @_emp_name, @_emp_code, @_salary)";
-
-    mysqlConnection.query(sql,[emp.id,emp.name,emp.emp_code,emp.salary],(err,rows,fields)=>{
-        if(!err){
-            //console.log(rows);
-        
-        res.send(rows);
-        }else{
-            console.log(err);
-        }
-    })
-});
+//         res.send(rows);
+//       } else {
+//         console.log(err);
+//       }
+//     }
+//   );
+// });
---
+++ db.js
@@ -0,0 +1,47 @@
+const msql = require("mysql");
+
+//===========================================================
+var mysqlConnection = msql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employeedb",
+  multipleStatements: true, //For use store procedure
+  connectionLimit: 50,
+});
+
+//======Create mysql Connection
+// mysqlConnection.connect((err) => {
+//   if (!err) {
+//     console.log("DB Connection Succcess!");
+//   } else {
+//     console.log("DB Connection Failed:" + err.message);
+//   }
+// });
+
+//
+//
+//
+//======Create Promise base funtion
+function query(quarySql) {
+  return new Promise((resolve, reject) => {
+    mysqlConnection.query(quarySql, function (err, results) {
+      //asynchronous funtion   function (err, results)
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+//======Create Promise base funtion
+function queryCb(quarySql, callback) {
+  mysqlConnection.query(quarySql, function (err, results) {
+    if (err) callback(err);
+    callback(null, results);
+  });
+}
+
+module.exports = { query, queryCb };
